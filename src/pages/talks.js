8782d49caa7f4babb11d28a31eb8bcf3efd22b96
Alexis Janvier <contact@alexisjanvier.net>
2018-03-04 11:28:59 +0100

    Update post workspace

---
+++ src/pages/talks.js
@@ -0,0 +1,54 @@
+/*
+Symfony2 CMF - la gestion de contenu bas-niveau
+Forum PHP 2013
+2013-11-21
+https://afup.org/pages/forumphp2013/sessions.php#867
+https://www.youtube.com/watch?v=1MlEsLbqqFw
+https://joind.in/event/forum-php-paris-2013/symfony2-cmf---la-gestion-de-contenu-bas-niveau
+
+Retour d'expérience sur le développement d'un service web
+Golang-Paris
+2014-11-28
+https://www.meetup.com/fr-FR/Golang-Paris/events/218803025/
+https://github.com/alexisjanvier/golang-paris
+
+Domptez vos API grâce à NG Admin !
+BarCamp PALO IT
+2015-12-02
+http://blog.palo-it.com/2015/11/16/barcamp-palo-it-domptez-vos-api-grace-a-ng-admin/
+https://www.youtube.com/watch?v=MPohj-bwapc
+http://slides.com/alexisjanvier-1/ng-admin
+
+Gérer ses serveurs de dev et test avec PM2
+ParisJs
+2016-02-24
+https://www.meetup.com/fr-FR/Paris-js/events/228786403/
+https://parisjs.org/meetup/2016-02-24/
+http://slides.com/alexisjanvier-1/manage-dev-test-servers-with-pm2
+
+Embauchez un service worker
+Rencontres Interactives
+2017-02-06
+https://www.youtube.com/watch?v=fgUYYV6Vvbc
+http://slides.com/alexisjanvier-1/embaucher-un-service-worker
+
+Le GraphQL, c'est vraiment la fin du REST ?
+CaenCamp
+2017-03-28
+http://www.caencamp.fr/talks/edition-28-le-graphql-cest-vraiment-la-fin-du-rest
+http://slides.com/alexisjanvier-1/graphql-4
+
+Je ne suis pas indépendant, mais je travaille à la maison
+ParisJs
+2017-06-28
+https://parisjs.org/meetup/2017-06-28/
+https://www.meetup.com/fr-FR/Paris-js/events/240727662/
+http://slides.com/alexisjanvier-1/full-remote
+
+Un monorepo pour les petits projets
+CaenCamp
+2018-02-20
+http://www.caencamp.fr/talks/edition-36-introduction-au-devops
+http://slides.com/alexisjanvier-1/monorepo
+
+*/
