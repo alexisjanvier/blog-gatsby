c9dc409b93c0609bb33a2091959c4f7092485189
Alexis Janvier <contact@alexisjanvier.net>
2018-07-25 08:30:24 +0200

    Get tags from existing posts

--- create-post.js
+++ create-post.js
@@ -13,42 +13,6 @@ inquirer.registerPrompt(
     require('inquirer-autocomplete-prompt')
 );
 
-const getDefaultAuthor = activeMembers => {
-    const hasDefaultAuthor = existsSync('./.authorrc');
-
-    if (hasDefaultAuthor) {
-        const defaultAuthor = readFileSync('./.authorrc', 'utf8');
-
-        if (activeMembers.includes(defaultAuthor)) {
-            return Promise.resolve(defaultAuthor);
-        }
-
-        console.log(chalk.red(`${defaultAuthor} is not a valid author.`));
-    }
-
-    return inquirer
-        .prompt([
-            {
-                type: 'autocomplete',
-                name: 'defaultAuthor',
-                message: `Who are you?`,
-                validate: input => Boolean(input) || 'Please answer',
-                source: (answers, input = '') =>
-                    Promise.resolve(
-                        input
-                            ? fuzzy
-                                .filter(input, activeMembers)
-                                .map(result => result.original)
-                            : activeMembers
-                    )
-            }
-        ])
-        .then(answers => {
-            writeFileSync('./.authorrc', answers.defaultAuthor, 'utf8');
-            return answers.defaultAuthor;
-        });
-};
-
 const getFrontMatterAttributes = file =>
     new Promise(resolve => {
         const content = readFileSync(file, 'utf8');
@@ -56,24 +20,12 @@ const getFrontMatterAttributes = file =>
         return resolve(data.attributes);
     });
 
-const getActiveTeamMembers = () => {
-    const files = readdirSync('./content/team');
-
-    return Promise.all(
-        files
-            .map(file => `./content/team/${file}`)
-            .map(getFrontMatterAttributes)
-    ).then(members =>
-        members.filter(member => !member.retired).map(member => member.name)
-    );
-};
-
 const getKnownTags = () => {
-    const files = readdirSync('./content/blog');
+    const files = readdirSync('./src/posts');
 
     return Promise.all(
         files
-            .map(file => `./content/blog/${file}`)
+            .map(file => `./src/posts/${file}`)
             .map(getFrontMatterAttributes)
     )
         .then(posts =>
@@ -83,7 +35,10 @@ const getKnownTags = () => {
                 new Set()
             )
         )
-        .then(tags => Array.from(tags));
+        .then(tags => Array.from(tags))
+        .then(tags => tags.map(tag => tag.toLowerCase()))
+        .then(tags => [...(new Set(tags))])
+        .then(tags => tags.sort());
 };
 
 const inquirerMoreTags = (postTags, tags) =>
@@ -143,79 +98,18 @@ const inquirerTags = (postTags, tags) =>
             return inquirerMoreTags(postTags, tags);
         });
 
-const inquirerMoreAuthors = (postAuthors, activeAuthors) =>
-    inquirer
-        .prompt([
-            {
-                type: 'autocomplete',
-                name: 'authors',
-                message: `Who are the authors? Current authors: ${
-                    postAuthors.length > 0 ? postAuthors.join(', ') : 'none'
-                }`,
-                suggestOnly: true,
-                pageSize: 10,
-                source: (answers, input = '') =>
-                    Promise.resolve(
-                        input
-                            ? fuzzy
-                                .filter(input, activeAuthors)
-                                .map(result => result.original)
-                            : activeAuthors
-                    )
-            },
-            {
-                type: 'confirm',
-                message: answers =>
-                    `Enter another author? Current authors: ${[
-                        ...postAuthors,
-                        answers.tag
-                    ].join(', ')}`,
-                name: 'enterAnotherAuthor'
-            }
-        ])
-        .then(answers => {
-            const newAuthors = [...postAuthors, answers.tag];
-            if (answers.enterAnotherAuthor) {
-                return inquirerAuthors(
-                    newAuthors,
-                    activeAuthors.filter(
-                        author => !newAuthors.includes(author)
-                    )
-                );
-            }
-            return newAuthors;
-        });
-
-const inquirerAuthors = (postAuthors, activeAuthors) =>
-    inquirer
-        .prompt([
-            {
-                type: 'confirm',
-                message: `Can we skip adding more authors? (Enter for yes)`,
-                name: 'skipAuthors'
-            }
-        ])
-        .then(answers => {
-            if (answers.skipAuthors) {
-                return postAuthors;
-            }
-            return inquirerMoreAuthors(postAuthors, activeAuthors);
-        });
-
-const createBlogpost = ({ answers, postAuthors, postTags }) => {
-    const { title, slug, excerpt } = answers;
+const createBlogpost = ({ answers, postTags }) => {
+    const { title, description } = answers;
     const currentDate = new Date();
     const currentFormattedDate = format(currentDate, 'YYYY-MM-DD');
-    const postPath = `./content/blog/${currentFormattedDate}-${slug}.md`;
+    const postPath = `./src/posts/${currentFormattedDate}-${slug}.md`;
 
     const content = `---
-layout: post
 title: "${title}"
-excerpt: "${excerpt}"
-image370x240_5:
-image1024x395:
-authors:
-${postAuthors.map(author => `- ${author}`).join('\n')}
+slug: "${slug(title)}"
+marmelab:
+date: "${currentFormattedDate}"
+description: "${description}"
 tags:
 ${postTags.map(tag => `- ${tag}`).join('\n')}
 ---
@@ -230,48 +124,52 @@ ${postTags.map(tag => `- ${tag}`).join('\n')}
     );
 };
 
-Promise.all([getActiveTeamMembers(), getKnownTags()]).then(
-    ([activeMembers, tags]) =>
-        getDefaultAuthor(activeMembers).then(defaultAuthor => {
-            inquirer
-                .prompt([
-                    {
-                        type: 'input',
-                        message: `Hey ${defaultAuthor}! What's your post title?`,
-                        name: 'title',
-                        validate: input => Boolean(input) || 'This is required'
-                    },
-                    {
-                        type: 'input',
-                        message: `What's the post slug?`,
-                        name: 'slug',
-                        default: answers =>
-                            slug(answers.title, { lower: true }),
-                        validate: input => Boolean(input) || 'This is required'
-                    },
-                    {
-                        type: 'input',
-                        message: `Write a short (usually 1-2 line) description of your post or press Enter to move on`,
-                        name: 'excerpt'
-                    }
-                ])
-                .then(answers =>
-                    inquirerAuthors(
-                        [defaultAuthor],
-                        activeMembers.filter(
-                            author => author !== defaultAuthor
-                        )
-                    ).then(postAuthors => ({
-                        answers,
-                        postAuthors
-                    }))
-                )
-                .then(result =>
-                    inquirerTags([], tags).then(postTags => ({
-                        ...result,
-                        postTags
-                    }))
-                )
-                .then(createBlogpost);
-        })
+Promise.all([getKnownTags()]).then(
+    ([tags]) => console.log(tags)
 );
+
+// Promise.all([getKnownTags()]).then(
+//     ([activeMembers, tags]) =>
+//         getDefaultAuthor(activeMembers).then(defaultAuthor => {
+//             inquirer
+//                 .prompt([
+//                     {
+//                         type: 'input',
+//                         message: `Hey ${defaultAuthor}! What's your post title?`,
+//                         name: 'title',
+//                         validate: input => Boolean(input) || 'This is required'
+//                     },
+//                     {
+//                         type: 'input',
+//                         message: `What's the post slug?`,
+//                         name: 'slug',
+//                         default: answers =>
+//                             slug(answers.title, { lower: true }),
+//                         validate: input => Boolean(input) || 'This is required'
+//                     },
+//                     {
+//                         type: 'input',
+//                         message: `Write a short (usually 1-2 line) description of your post or press Enter to move on`,
+//                         name: 'excerpt'
+//                     }
+//                 ])
+//                 .then(answers =>
+//                     inquirerAuthors(
+//                         [defaultAuthor],
+//                         activeMembers.filter(
+//                             author => author !== defaultAuthor
+//                         )
+//                     ).then(postAuthors => ({
+//                         answers,
+//                         postAuthors
+//                     }))
+//                 )
+//                 .then(result =>
+//                     inquirerTags([], tags).then(postTags => ({
+//                         ...result,
+//                         postTags
+//                     }))
+//                 )
+//                 .then(createBlogpost);
+//         })
+// );
